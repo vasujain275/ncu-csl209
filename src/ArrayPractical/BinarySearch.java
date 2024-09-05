d3cf79a7b5b82234ccbad4954c2dd6c3e10bcd09
vasujain275 <vasujain275@gmail.com>
2024-09-05 11:15:42 +0530

    Refactor

--- src/ArrayPractical/BinarySearch.java
+++ src/ArrayPractical/BinarySearch.java
@@ -1,4 +1,38 @@
 package ArrayPractical;
 
 public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {2, 5, 10, 18, 23, 33, 40};
+        int target = 23;
+
+        int result = binarySearch(array, target);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
 }
--- src/ArrayPractical/LinearSearch.java
+++ src/ArrayPractical/LinearSearch.java
@@ -1,4 +1,26 @@
 package ArrayPractical;
 
 public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {10, 5, 23, 2, 33, 18, 40};
+        int target = 23;
+
+        int result = linearSearch(array, target);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
 }
